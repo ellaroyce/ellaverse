4a9b63a47c7cd032f1ea248224e2e0e0834ec12e
Ella <eyatyuryumina@gmail.com>
2026-02-02 10:59:40 +0100

    [mistake fix]

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,20 @@ import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   base: "/ellaverse/",
+
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: { overlay: false },
   },
 }));
