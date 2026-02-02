32a35ae294718da024cee69e020cbb68fb65e120
Ella <eyatyuryumina@gmail.com>
2026-02-02 08:27:46 +0100

    upd config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  base: "/ellaverse/", // <-- replace with your repo name
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: "/ellaverse/", // <- MUST match your repo name
+});
